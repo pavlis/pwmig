74ee13bec2443377aa93ec0d2c3a276d8e49b5df
Gary Pavlis <pavlis@indiana.edu>
2014-08-05 05:59:19 -0400

    Major addition to support vector fields and component names.
    This is not a working version but did this commit to save this version
    that attempted to use polymophism.   Generating compile message so will
    take a different strategy and use a template.

--- paraview/gclfield2vtk/vtk_output.cc
+++ paraview/gclfield2vtk/vtk_output.cc
@@ -10,7 +10,13 @@
 
 using namespace std;
 
-vtkStructuredGrid*	convert_gcl3d_to_vtksg(GCLscalarfield3d &g)
+/* g is the grid to be converted.   
+
+   tag is a string vector whose first component is uses as the name field for the
+   scalars field in the output data file.   It is a vector to allow polymorphic
+   implementaiton of the writer later in this file.  Only the first element of the
+   tag vector is accessed by this procedure.*/
+vtkStructuredGrid*	convert_gcl3d_to_vtksg(GCLscalarfield3d &g, vector<string> tag)
 {
 
 	vtkStructuredGrid	*pGrid 			= vtkStructuredGrid::New();
@@ -21,6 +27,7 @@ vtkStructuredGrid*	convert_gcl3d_to_vtksg(GCLscalarfield3d &g)
 
 	pData->SetNumberOfTuples(g.n1 * g.n2 * g.n3);
 	pData->SetNumberOfComponents(1);
+        pData->SetComponentName(0,tag[0].c_str());
 
 	int kk;
 	for(int k=g.n3-1,kk=0;k>=0;k--,kk++)
@@ -47,15 +54,75 @@ vtkStructuredGrid*	convert_gcl3d_to_vtksg(GCLscalarfield3d &g)
 
 	return(pGrid);
 }
+/* g is the vectofield data to be converted.  
+   tags is the a vector of strings to set as the component names in the vtk array.   
+   length of tags array must match g.nv.  */
+vtkStructuredGrid*	convert_gcl3d_to_vtksg(GCLvectorfield3d &g,vector<string> tags)
+{
+
+	vtkStructuredGrid	*pGrid 			= vtkStructuredGrid::New();
+	vtkPoints		*pPoints		= vtkPoints::New();
+	vtkDoubleArray		*pData			= vtkDoubleArray::New();
+
+	pGrid->SetDimensions(g.n1, g.n2, g.n3);
+
+	pData->SetNumberOfComponents(g.nv);
+	pData->SetNumberOfTuples(g.n1 * g.n2 * g.n3);
+	int k,kk,l;
+        string null_name("component");
+        if( (g.nv) != (tags.size()) )
+            cerr << "Warning (convert_gcl3d_to_vtksg):  Mismatch in vector field component names"
+                                                        <<endl
+                << "Field be converted has "<<g.nv<<" components"<<endl
+                << "Received a vector of strings of length "<<tags.size()<<endl
+                << "Any undefined names will get the tag = "<<null_name<<endl;
+        for(k=0;k<g.nv;++k)
+        {
+            if(k<tags.size())
+            {
+                pData->SetComponentName(k,tags[k].c_str());
+            }
+            else
+            {
+                pData->SetComponentName(k,null_name.c_str());
+            }
+        }
+
+	for(k=g.n3-1,kk=0;k>=0;k--,kk++)
+	{
+		for(int j = 0; j < g.n2; j++)
+		{
+			for(int i = 0; i < g.n1; i++)
+			{
+				pPoints->InsertNextPoint(g.x1[i][j][k], 
+					g.x2[i][j][k], g.x3[i][j][k]);
+
+                                for(l=0;l<g.nv;++l)
+                                    pData->SetComponent((kk*g.n1*g.n2)+(j*g.n1)+i,
+                                            l,g.val[i][j][k][l]);
+				
+			}
+		}
+	}
+
+	pGrid->SetPoints(pPoints);
+	pGrid->GetPointData()->SetScalars(pData);
+
+	pPoints->Delete();
+	pData->Delete();
+
+	return(pGrid);
+}
 
 
-void output_gcl3d_to_vtksg(GCLscalarfield3d &g, const string filename,
+void output_gcl3d_to_vtksg(GCLgrid3d &g, const string filename,
+                vector<string> tags,
 		bool use_xml = false, bool use_binary=true)
 {
 	
 	vtkStructuredGrid *pGrid = 0; 	
 
-	pGrid = convert_gcl3d_to_vtksg(g);
+	pGrid = convert_gcl3d_to_vtksg(g,tags);
 	if(use_xml)
 	{
 		vtkXMLStructuredGridWriter *pWriter
--- paraview/gclfield2vtk/vtk_output.h
+++ paraview/gclfield2vtk/vtk_output.h
@@ -4,23 +4,33 @@
 
 class vtkStructuredGrid; 	// forward reference
 
-// @{
-// Convert a GCLscalarfield3d object to a VTK Structured Grid written to disk.
-//
-// @param g The grid object to convert from.
-// @param filename Filename to write to.
-// @param use_xml Boolean flag to determine if the new XML VTK format should be used. (default = false)
-// @param swap_xz Swap X/Z values to fit a "z out" coordinate arrangement. (default = true)
-//
-// @}
-void	output_gcl3d_to_vtksg(GCLscalarfield3d &g, const string filename, bool use_xml = false,
-						 bool use_binary=true);
+/*!/ \brief Convert a GCLscalarfield3d object to a VTK Structured Grid written to disk.
 
-// @{
-// Convert a GCLscalarafield3d object to a vtkStructuredGrid object
-//
-// @param g The grid object to convert from.
-// @}
-vtkStructuredGrid *convert_gcl3d_to_vtksg(GCLscalarfield3d &g);
+ \param g The grid object to be written.  Polymorphism assumed as their are converters
+       for both vector and scalar objects.
+ \param filename Filename to write to.
+ \param use_xml Boolean flag to determine if the new XML VTK format should be used. (default = false)
+ \param swap_xz Swap X/Z values to fit a "z out" coordinate arrangement. (default = true)
+
+*/
+void	output_gcl3d_to_vtksg(GCLgrid3d &g, const string filename, vector<string> tags, 
+        bool use_xml = false,bool use_binary=true);
+
+/*! \brief Convert a GCLscalarafield3d object to a vtkStructuredGrid object
+
+\param g The grid object to convert from.
+\param tags is assumed to contain a single string component that is the name assigned
+   to the scalars field of the output.
+*/
+vtkStructuredGrid *convert_gcl3d_to_vtksg(GCLscalarfield3d &g,vector<string> tags);
+/*! \brief Convert a GCLvectorafield3d object to a vtkStructuredGrid object
+
+\param g The grid object to convert from.
+\param tags is a vector of strings that tag the attributes in each component of the
+   vector field that is to be converted.  The size of this vector should match the
+   nv attribute of the vector field.   This is not a fatal error but will generate 
+   a warning if the sizes do that match.
+*/
+vtkStructuredGrid *convert_gcl3d_to_vtksg(GCLvectorfield3d &g, vector<string> tags);
 
 #endif
