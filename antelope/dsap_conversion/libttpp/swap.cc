e7db0cc3490e4e070208c3e5cb1c6a11440a197b
Gary Pavlis <pavlis@indiana.edu>
2014-12-20 06:44:35 -0500

    Get ride of baggage swap_gcc.cc

--- antelope/dsap_conversion/libttpp/swap.cc
+++ antelope/dsap_conversion/libttpp/swap.cc
@@ -9,33 +9,34 @@ using namespace std;
    length n*/
 void vector_swap4(int *in,int *out,int n)
 {
-    u_int32_t so;  //so is short for swapped output
+    int32_t so;  //so is short for swapped output
     int i;
     for(i=0;i<n;++i)
     {
-        so=be32toh(static_cast<u_int32_t>(in[i]));
+        so=__builtin_bswap32(static_cast<int32_t>(in[i]));
         out[i]=static_cast<int>(so);
     }
 }
 void md2hd(double *in, double *out,int n)
 {
-    u_int64_t so;
+    int64_t so;
     int i;
     for(i=0;i<n;++i)
     {
-        so=be64toh(static_cast<u_int64_t>(in[i]));
+        so=__builtin_bswap64(static_cast<int64_t>(in[i]));
         out[i]=static_cast<double>(so);
     }
 }
-/*Added this one for efficiency.  The taup f2c junk uses a define to
-for a SWAP4 function with a single argument to swap one 32 bit integer.
-With C++ we can use overloading to avoid the define.
-*/
 void swap4(int *d)
 {
-    u_int32_t so;
-    so=be32toh(static_cast<u_int32_t>(*d));
+    int32_t so;
+    so=__builtin_bswap32(static_cast<int32_t>(*d));
     *d=static_cast<int>(so);
 }
-
+void swap4_float(float *d)
+{
+    int32_t so;
+    so=__builtin_bswap32(static_cast<int32_t>(*d));
+    *d=static_cast<int>(so);
+}
 
