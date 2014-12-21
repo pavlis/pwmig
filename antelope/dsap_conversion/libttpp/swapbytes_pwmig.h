716ff86bfa0d51428d156756044b4e9be6c49f62
Gary Pavlis <pavlis@indiana.edu>
2014-12-21 06:49:01 -0500

    Add missing ifdefs for c++

--- antelope/dsap_conversion/libttpp/swapbytes_pwmig.h
+++ antelope/dsap_conversion/libttpp/swapbytes_pwmig.h
@@ -1,6 +1,12 @@
 /* This is a local include file to define glp implementation of 
    swapbyte routines in antelope.   Needed only for prototypes.*/
+#ifdef  __cplusplus
+extern "C" {
+#endif
 void vectorswap4(int *in,int *out,int n);
 void swap4(int *d);
 void swap4_float(float *d);
 void md2hd(double *in, double *out,int n);
+#ifdef  __cplusplus
+}
+#endif
