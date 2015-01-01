1208a493e83b6194bef33e190e1ec427b06b04d9
Gary Pavlis <pavlis@indiana.edu>
2015-01-01 09:26:16 -0500

    more changes trying to get pwstack to link

--- antelope/dsap_conversion/libstock/elog.h
+++ antelope/dsap_conversion/libstock/elog.h
@@ -7,6 +7,9 @@ void elog_log( int flag, const char *format, ... );
 void elog_notify( int flag, const char *format, ... );
 void elog_complain( int flag, const char *format, ... );
 void elog_die( int flag, const char *format, ... );
+#define register_error(x) elog_notify(x)
+#define complain(x) elog_complain(x)
+#define die(x) elog_die(x)
 #ifdef  __cplusplus
 }
 #endif
